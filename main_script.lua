ccfe80630010fe48ac76d0ab60fc44feef28d5a1
David <40366903+DavidTDC3377@users.noreply.github.com>
2023-02-28 11:35:29 +0000

    Updated main script
    I have changed the string for the shutdown message as \n is not supported.
    
    See https://devforum.roblox.com/t/newline-character-doesnt-work-on-kick-messages/1885566/12 for more info

--- main_script.lua
+++ main_script.lua
@@ -1,3 +1,4 @@
+local pre = "BA_RMD_"
 --[[
 	
 	
@@ -11,7 +12,6 @@
 	Please note that BAE was created by TheFurryFish, Basic Admin Remade
 	was made by B00PUP.
 	
-  This GitHub fork is by DavidTDC3377/DavidStudios
 	
 	Instructions:
 	Place this Model in
@@ -61,7 +61,7 @@ local Configuration = {
 
 	
 ['Super Admins'] = {
-		[1275376431] = "B00PUP",
+		[1] = "Roblox",
 		
 
 	},
@@ -80,7 +80,7 @@ local Configuration = {
 	
 	['Group Configuration'] = {
 		{
-			['Group ID'] = 0,
+			['Group ID'] = 0, 
 			['Group Rank'] = 0,
 			['Tolerance Type'] = '>=',
 			['Admin Level'] = 0,
@@ -100,22 +100,22 @@ local Configuration = {
 	['Prefix'] = (':'), -- The prefix to the admin, i.e :cmds or :sm hi
 	['Kick Reason'] = ('You have been kicked from the server.'), -- Displayed to people that are kicked.
 	['Ban Reason'] = ('You have been banned from the game.'), -- Displayed to people that are banned.
-	['Shutdown Reason'] = ('This server is shutting down..\nTry joining a different server!'), -- Displayed to people when the server is shut down.
+	['Shutdown Reason'] = ('This server is shutting down.. Try joining a different server!'), -- Displayed to people when the server is shut down.
 	['Server Message'] = ('BAR Server Message'), -- Displayed in the :sm command's title.
-	['Server Lock Reason'] = ('The server is locked.'), -- Displayed to people being kicked that try to enter a locked server.
+	['Server Lock Reason'] = ('The server is locked.'), -- Displayed to people being kicked when they try to enter a locked server.
 	['Trello'] = false, -- Use trello? HttpService must be enabled.
 	['Trello Board'] = (''), -- Trello board ID.
 	['Trello App Key'] = (''), -- Private trello application key.
 	['Trello Token'] = (''), -- Private trello token.
-	['Creator Debugging'] = true, -- Allows the creator to debug potential issues with the admin.
+	['Creator Debugging'] = false, -- Allows the creator to debug potential issues with the admin.
 	['Donor Perks'] = true, -- Gives people who purchase an admin donation the ability to cape, put hats on, etc.
 	['Public Commands'] = false, -- Will people that are not admin be able to say :cmds, or !clean?
 	['Auto Clean'] = true, -- Will hats and gear automatically be cleaned up every so often?
 	['System Color'] = Color3.new(0.333333, 0.666667, 1), -- Color of the admin's guis.
 	['Tools Location'] = game.ServerStorage, -- Where the :tools and :give command will get tools from.
 	['Command Confirmation'] = true, -- Validates certain commands like :Ban all, or :PBan all.
-	['Datastore Key'] = ('BAR') -- What cape data, ban data, etc. is stored under. Changing will wipe it.
-}
+	['Datastore Key'] = (pre.."1") -- What cape data, ban data, etc. is stored under. Changing will wipe it.
+}       -- NOTE: Changing 1 in Datastore Key will wipe bans and other data unless you change it back!
 
 --------------------------
 -- End of Configuration --
